ea7b6a818a389de4be165b1aa0c5ee29c0b71d84
Gulnara Che <g.chepurnova@gmail.com>
2023-11-14 19:06:04 +0500

    updt

--- src/test/java/org/example/Statistics/StatisticsServiceTest.java
+++ src/test/java/org/example/Statistics/StatisticsServiceTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class StatisticsServiceTest {
 
     @Test
-    void findMaximum() {
+    void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
         long actual = service.findMax(incomesInBillions);
-        System.out.println("Максимальное число: " + actual);
 
         Assertions.assertEquals(expected, actual);
     }
